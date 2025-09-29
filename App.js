baf58d2d9e22638175e929356da66e87c691deba
B00164804 Ledes Ajuonoma <B00164804@mytudublin.ie>
2025-09-29 16:04:57 +0100

    Step 2

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import Logo from './components/Logo';
 
 export default function App(){
   const [fullname, setFullname] = useState("Eoin");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
+
   return(
     <View> 
       <Logo/>
       <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+      <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
     </View>
   )
 };
-
 
