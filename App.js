fab554c6b5470dcdc4b6e3b8652bb9976259dcfb
B00164804 Ledes Ajuonoma <B00164804@mytudublin.ie>
2025-09-29 15:55:30 +0100

    Step 1

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
-
+import Logo from './components/Logo';
 
 export default function App(){
   const [fullname, setFullname] = useState("Eoin");
   return(
     <View> 
+      <Logo/>
       <Text>Hello, World {fullname}</Text>
       <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
     </View>
   )
 };
-
 
 
